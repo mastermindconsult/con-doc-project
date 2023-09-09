a5ce001262b106bd56e854b18fad980890ae3f52
enochsedwards <enoch.s.edwards@gmail.com>
2023-09-09 13:04:24 +0100

    modified alb_listener_port_80.tf

--- Dev/alb_listener_port_80.tf
+++ Dev/alb_listener_port_80.tf
@@ -4,7 +4,7 @@ resource "aws_lb_listener" "alb-http-listener" {
   port              = 80
   protocol          = "HTTP"
 
-   default_action {
+    default_action {
     type             = "forward"
     target_group_arn = aws_lb_target_group.lb-target-group.arn
   }
