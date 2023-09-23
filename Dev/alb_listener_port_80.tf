4a96e18c82f2abdc3701815ce211594e3d5eb942
wagmiexchange <wagmiexchange@gmail.com>
2023-09-23 16:52:38 +0100

    QC Updates - ALB

--- Dev/alb_listener_port_80.tf
+++ Dev/alb_listener_port_80.tf
@@ -1,4 +1,5 @@
 # create a listener on port 80 with forward action
+
 resource "aws_lb_listener" "alb-http-listener" {
   load_balancer_arn = aws_lb.load-balancer.arn
   port              = 80
@@ -8,4 +9,4 @@ resource "aws_lb_listener" "alb-http-listener" {
     type             = "forward"
     target_group_arn = aws_lb_target_group.lb-target-group.arn
   }
-} 
+}
--- Dev/alb_target_group.tf
+++ Dev/alb_target_group.tf
@@ -1,11 +1,12 @@
 # create ECS ALB target group with HTTP protocol (port 80)
+
 resource "aws_lb_target_group" "lb-target-group" {
   name        = "${var.project_name}-lb-target-group"
   target_type = "ip"
   port        = 80
   protocol    = "HTTP"
   vpc_id      = aws_vpc.vpc.id
-  
+
 
   health_check {
     enabled             = true
